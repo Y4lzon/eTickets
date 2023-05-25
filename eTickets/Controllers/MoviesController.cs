bafacd7cda911e63cb26a080ed16d4cb7f3e0021
Yelizar Sezon <sezon2004@gmail.com>
2023-05-24 22:42:41 -0700

    Adding new movie to database

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -45,6 +46,24 @@ namespace eTickets.Controllers
             ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewMovieVM movie)
+        {
+            if(!ModelState.IsValid)
+            {
+                var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+                ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+                ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+                ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+                return View(movie);
+            }
+
+            await _service.AddNewMovieAsync(movie);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eTickets/Data/Services/IMoviesService.cs
+++ eTickets/Data/Services/IMoviesService.cs
@@ -8,7 +8,10 @@ namespace eTickets.Data.Services
 	public interface IMoviesService : IEntityBaseRepository<Movie>
 	{
 		Task<Movie> GetMovieByIdAsync(int id);
+
 		Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+
+		Task AddNewMovieAsync(NewMovieVM data);
 	}
 }
 
