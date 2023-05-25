88b211541c704743d52ae20c34de4e31c4c10b68
Yelizar Sezon <sezon2004@gmail.com>
2023-05-25 09:01:03 -0700

    Adding Order & OrderItem models

---
+++ eTickets/Models/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+	public class OrderItem
+	{
+        [Key]
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+        public double Price { get; set; }
+
+        public int MovieId { get; set; }
+        [ForeignKey("MovieId")]
+        public Movie Movie { get; set; }
+
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+
+        public OrderItem()
+		{
+		}
+	}
+}
+
