4efadfead010f1279c9b0d173a8f29c78fd26a44
Yelizar Sezon <sezon2004@gmail.com>
2023-05-25 09:43:32 -0700

    Adding OrdersController file
    - Adding ShoppingCartVM file

---
+++ eTickets/Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: /<controller>/
+        private readonly IMoviesService _movieService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService movieService, ShoppingCart shoppingCart)
+        {
+            _movieService = movieService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
+
---
+++ eTickets/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using eTickets.Data.Cart;
+
+namespace eTickets.Data.ViewModels
+{
+	public class ShoppingCartVM
+	{
+		public ShoppingCart ShoppingCart { get; set; }
+
+		public double ShoppingCartTotal { get; set; }
+	}
+}
+
