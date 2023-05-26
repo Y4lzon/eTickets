87ff9dfa4249ba679ced23480faae8161e1822dc
Yelizar Sezon <sezon2004@gmail.com>
2023-05-25 18:57:56 -0700

    Designing Sign up view

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -52,6 +52,7 @@ namespace eTickets.Controllers
             TempData["Error"] = "Wrong credentials. Please, try again!";
             return View(loginVM);
         }
+        public IActionResult Register() => View(new RegisterVM());
     }
 }
 
